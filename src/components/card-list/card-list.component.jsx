3d1e5af0bbdd5be42ddf7544e48da0447300275b
Santiago <santiagomorales333@yahoo.com>
2023-04-14 10:00:53 -0700

    completed monsters rolodex

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import Card from "./card.componet";
+import Card from "../card/card.componet";
 import "./card-list.styles.css";
 
 class CardList extends Component {
@@ -8,8 +8,7 @@ class CardList extends Component {
     return (
       <div className="card-list">
         {monsters.map((monster) => {
-          const { name, id, email } = monster;
-          return <Card name={name} id={id} email={email} />;
+          return <Card monster={monster} />;
         })}
       </div>
     );
